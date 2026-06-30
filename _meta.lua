dd73f5529777340000f227edf335565dd69dfe59
Reuben Sinha <reuben.sinha@gmail.com>
2026-06-29 21:52:33 -0700

    Fix: change versioning convention from tuple to string.

--- _meta.lua
+++ _meta.lua
@@ -10,7 +10,7 @@ return {
     name = "jnc-reader",
     fullname = "JNC Reader",
     description = [[Read J-Novel Club subscription content directly in KOReader.]],
-    version = {0, 2, 1},
+    version = "0.2.1",
     author = "Reuben Sinha",
     url = "https://github.com/reubensinha/jnc-reader.koplugin",
     license = "MIT",
